01e4c092b575db5d5163d78925909aa3c2d881c9
donhui <977675308@qq.com>
2019-08-29 09:30:37 +0800

    not validate external dtd when parse xml file

--- src/main/java/org/sonarsource/plugins/mybatis/xml/XmlParser.java
+++ src/main/java/org/sonarsource/plugins/mybatis/xml/XmlParser.java
@@ -1,14 +1,26 @@
 package org.sonarsource.plugins.mybatis.xml;
 
+import com.sun.org.apache.xerces.internal.impl.Constants;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.io.SAXReader;
+import org.sonar.api.utils.log.Logger;
+import org.sonar.api.utils.log.Loggers;
+import org.xml.sax.SAXException;
 
 import java.io.File;
 
 public class XmlParser {
+    private static final Logger LOGGER = Loggers.get(XmlParser.class);
+
     public Document parse(File file) throws DocumentException {
         SAXReader reader = new SAXReader();
+        reader.setValidation(false);
+        try {
+            reader.setFeature(Constants.XERCES_FEATURE_PREFIX + Constants.LOAD_EXTERNAL_DTD_FEATURE, false);
+        } catch (SAXException e) {
+            LOGGER.warn(e.toString());
+        }
         return reader.read(file);
     }
 }
