7274b85a5de1ee59ea4389fbfc2790671ad9a2dc
donhui <977675308@qq.com>
2019-08-31 15:17:07 +0800

    bugfix: fix NullPointerException on Windows

--- src/main/java/org/sonarsource/plugins/mybatis/rules/MyBatisLintSensor.java
+++ src/main/java/org/sonarsource/plugins/mybatis/rules/MyBatisLintSensor.java
@@ -107,14 +107,14 @@ public class MyBatisLintSensor implements Sensor {
                 if ("mapper".equals(rootElement.getName()) && publicIdOfDocType.contains("mybatis.org")) {
                     LOGGER.info("handle mybatis mapper xml:" + xmlFilePath);
                     // handle mybatis mapper file
-                    String dstXmlFilePath = xmlFilePath + "-reduced.xml";
-                    File dstXmlFile = new File(dstXmlFilePath);
-                    reducedFileList.add(dstXmlFile);
+                    String reducedXmlFilePath = xmlFilePath + "-reduced.xml";
+                    File reducedXmlFile = new File(reducedXmlFilePath);
+                    reducedFileList.add(reducedXmlFile);
                     MyBatisMapperXmlHandler myBatisMapperXmlHandler = new MyBatisMapperXmlHandler();
-                    myBatisMapperXmlHandler.handleMapperFile(xmlFile, dstXmlFile);
-                    mybatisMapperMap.put(dstXmlFilePath, xmlFilePath);
+                    myBatisMapperXmlHandler.handleMapperFile(xmlFile, reducedXmlFile);
+                    mybatisMapperMap.put(reducedXmlFilePath, xmlFilePath);
                     // xmlMapperBuilder parse mapper resource
-                    Resource mapperResource = new FileSystemResource(dstXmlFile);
+                    Resource mapperResource = new FileSystemResource(reducedXmlFile);
                     XMLMapperBuilder xmlMapperBuilder = new XMLMapperBuilder(mapperResource.getInputStream(),
                         mybatisConfiguration, mapperResource.toString(), mybatisConfiguration.getSqlFragments());
                     xmlMapperBuilder.parse();
@@ -158,9 +158,16 @@ public class MyBatisLintSensor implements Sensor {
                             sql = sql.replaceAll("\\n", "");
                             sql = sql.replaceAll("\\s{2,}", " ");
                             String mapperResource = stmt.getResource();
-                            String dstMapperFilePath =
+                            String reducedXmlFilePath =
                                 mapperResource.substring(mapperResource.indexOf('[') + 1, mapperResource.indexOf(']'));
-                            String sourceMapperFilePath = (String)mybatisMapperMap.get(dstMapperFilePath);
+
+                            // windows environment
+                            if(!reducedXmlFilePath.startsWith("/")){
+                                reducedXmlFilePath = "/" + reducedXmlFilePath.replace("\\", "/");
+                            }
+                            LOGGER.debug("reducedMapperFilePath: " + reducedXmlFilePath);
+
+                            String sourceMapperFilePath = (String)mybatisMapperMap.get(reducedXmlFilePath);
 
                             LOGGER.info("id=" + stmtId + ",");
                             LOGGER.info("sql=" + sql);
@@ -174,6 +181,7 @@ public class MyBatisLintSensor implements Sensor {
 
                                 String sqlCmdType = stmt.getSqlCommandType().toString().toLowerCase();
 
+                                LOGGER.debug("sourceMapperFilePath: " + sourceMapperFilePath);
                                 Integer lineNumber = getLineNumber(sourceMapperFilePath, stmtIdTail, sqlCmdType);
 
                                 // match Rule And Save Issue
@@ -191,7 +199,7 @@ public class MyBatisLintSensor implements Sensor {
         for (File file : files) {
             if (file.exists() && file.isFile()) {
                 try {
-                    Files.delete(Paths.get(new URI("file:///" + file.getAbsolutePath())));
+                    Files.delete(Paths.get(new URI("file:///" + file.getAbsolutePath().replace("\\","/"))));
                 } catch (IOException | URISyntaxException e) {
                     LOGGER.warn(e.toString());
                 }
