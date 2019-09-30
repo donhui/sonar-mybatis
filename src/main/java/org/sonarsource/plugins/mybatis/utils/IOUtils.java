344d5685c0b1f022e692ccb8095e3b8ccdde0948
Bastian Bastias Sanchez <bastian.bastias@consorcio.cl>
2019-09-30 13:29:53 -0300

    xml resource reading optimization, now opens only once per sql sentence search with single and double quotes. It no longer returns a null object in case the file has a different format and did not find the statement.

--- src/main/java/org/sonarsource/plugins/mybatis/utils/IOUtils.java
+++ src/main/java/org/sonarsource/plugins/mybatis/utils/IOUtils.java
@@ -1,40 +1,53 @@
 package org.sonarsource.plugins.mybatis.utils;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.nio.charset.StandardCharsets;
+
 import org.sonar.api.utils.log.Logger;
 import org.sonar.api.utils.log.Loggers;
 
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-
 public class IOUtils {
 
-    private IOUtils() {}
+    private IOUtils() {
+        super();
+    }
 
     private static final Logger LOGGER = Loggers.get(IOUtils.class);
 
     /**
-     * search file, get the line number which matches the keyword firstly
+     * Search file, get the line number which matches the keyword firstly
      * 
      * @param filePath
-     * @param keyWord1
-     * @param keyWord2
+     * @param stmtId
+     * @param sqlCmdType
      * @return
      */
-    public static Integer getLineNumber(String filePath, String keyWord1, String keyWord2) {
-        Integer lineNumber = null;
-        // lineNumberReader
-        try (LineNumberReader lineNumberReader =
-            new LineNumberReader(new InputStreamReader(new FileInputStream(filePath), StandardCharsets.UTF_8))) {
+    public static int getLineNumber(final String filePath, final String stmtId, final String sqlCmdType) {
+        int lineNumber = 1;// numero mayor a cero en caso que "no se llame"
+        final String sqlCmdTypeAux = "<" + sqlCmdType;
+        final String stmtIdAuxDoublequote = stmtId + "\"";
+        final String stmtIdAuxSimplequote = stmtId + "\'";
+        LOGGER.debug("filePath: " + filePath + " stmtId: " + stmtId + " sqlCmdType: " + sqlCmdType);
+        try (LineNumberReader lineNumberReader = new LineNumberReader(
+                new InputStreamReader(new FileInputStream(filePath), StandardCharsets.UTF_8))) {
             String readLine = null;
             while ((readLine = lineNumberReader.readLine()) != null) {
                 // check if contains
-                if (readLine.contains(keyWord1) && readLine.contains(keyWord2)) {
+                if (readLine.toLowerCase().contains(sqlCmdTypeAux)
+                        && (readLine.contains(stmtIdAuxDoublequote) || readLine.contains(stmtIdAuxSimplequote))) {
                     lineNumber = lineNumberReader.getLineNumber();
                     break;
                 }
             }
         } catch (IOException e) {
-            LOGGER.warn(e.toString());
+            LOGGER.error(e.toString());
+        }
+        LOGGER.debug("lineNumber found: " + lineNumber);
+        if (0 == lineNumber) {
+            LOGGER.warn("dosnt found line number.");
         }
         return lineNumber;
     }
