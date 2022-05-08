f670cee62dba98cd8de4c4d8917c5cc003b02cb7
donhui <977675308@qq.com>
2022-05-08 21:50:05 +0800

    support sonarqube 9.0

--- src/main/java/org/sonarsource/plugins/mybatis/languages/MyBatisQualityProfile.java
+++ src/main/java/org/sonarsource/plugins/mybatis/languages/MyBatisQualityProfile.java
@@ -16,25 +16,25 @@ public final class MyBatisQualityProfile implements BuiltInQualityProfilesDefini
         NewBuiltInQualityProfile profile = context.createBuiltInQualityProfile("MyBatisLint Rules", Xml.KEY);
         profile.setDefault(true);
 
-        NewBuiltInActiveRule rule1 = profile.activateRule(REPO_KEY, "MyBatisMapperCheckRule1");
+        NewBuiltInActiveRule rule1 = profile.activateRule(REPO_KEY, "MyBatisMapperCheckRule01");
         rule1.overrideSeverity(Severity.MINOR);
 
-        NewBuiltInActiveRule rule2 = profile.activateRule(REPO_KEY, "MyBatisMapperCheckRule2");
+        NewBuiltInActiveRule rule2 = profile.activateRule(REPO_KEY, "MyBatisMapperCheckRule02");
         rule2.overrideSeverity(Severity.MAJOR);
 
-        NewBuiltInActiveRule rule3 = profile.activateRule(REPO_KEY, "MyBatisMapperCheckRule3");
+        NewBuiltInActiveRule rule3 = profile.activateRule(REPO_KEY, "MyBatisMapperCheckRule03");
         rule3.overrideSeverity(Severity.CRITICAL);
 
-        NewBuiltInActiveRule rule4 = profile.activateRule(REPO_KEY, "MyBatisMapperCheckRule4");
+        NewBuiltInActiveRule rule4 = profile.activateRule(REPO_KEY, "MyBatisMapperCheckRule04");
         rule4.overrideSeverity(Severity.MINOR);
 
-        NewBuiltInActiveRule rule5 = profile.activateRule(REPO_KEY, "MyBatisMapperCheckRule5");
+        NewBuiltInActiveRule rule5 = profile.activateRule(REPO_KEY, "MyBatisMapperCheckRule05");
         rule5.overrideSeverity(Severity.MAJOR);
 
-        NewBuiltInActiveRule rule6 = profile.activateRule(REPO_KEY, "MyBatisMapperCheckRule6");
+        NewBuiltInActiveRule rule6 = profile.activateRule(REPO_KEY, "MyBatisMapperCheckRule06");
         rule6.overrideSeverity(Severity.CRITICAL);
 
-        NewBuiltInActiveRule rule7 = profile.activateRule(REPO_KEY, "MyBatisMapperCheckRule7");
+        NewBuiltInActiveRule rule7 = profile.activateRule(REPO_KEY, "MyBatisMapperCheckRule07");
         rule7.overrideSeverity(Severity.MINOR);
 
         profile.done();
--- src/main/java/org/sonarsource/plugins/mybatis/rules/MyBatisLintRulesDefinition.java
+++ src/main/java/org/sonarsource/plugins/mybatis/rules/MyBatisLintRulesDefinition.java
@@ -2,37 +2,98 @@ package org.sonarsource.plugins.mybatis.rules;
 
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+
+import org.sonar.api.rule.RuleKey;
+import org.sonar.api.rule.Severity;
+import org.sonar.api.rules.RuleType;
 import org.sonar.api.server.rule.RulesDefinition;
 import org.sonar.api.server.rule.RulesDefinitionXmlLoader;
 import org.sonar.plugins.xml.Xml;
 
 public final class MyBatisLintRulesDefinition implements RulesDefinition {
 
-    private static final String PATH_TO_RULES_XML = "/mybatis/mybatislint-rules.xml";
-
     public static final String REPO_KEY = "MyBatisLint";
     protected static final String REPO_NAME = REPO_KEY;
 
-    protected String rulesDefinitionFilePath() {
-        return PATH_TO_RULES_XML;
-    }
+    public static final RuleKey MYBATIS_MAPPER_CHECK_RULE_01 = RuleKey.of(REPO_KEY, "MyBatisMapperCheckRule01");
+    public static final RuleKey MYBATIS_MAPPER_CHECK_RULE_02 = RuleKey.of(REPO_KEY, "MyBatisMapperCheckRule02");
+    public static final RuleKey MYBATIS_MAPPER_CHECK_RULE_03 = RuleKey.of(REPO_KEY, "MyBatisMapperCheckRule03");
+    public static final RuleKey MYBATIS_MAPPER_CHECK_RULE_04 = RuleKey.of(REPO_KEY, "MyBatisMapperCheckRule04");
+    public static final RuleKey MYBATIS_MAPPER_CHECK_RULE_05 = RuleKey.of(REPO_KEY, "MyBatisMapperCheckRule05");
+    public static final RuleKey MYBATIS_MAPPER_CHECK_RULE_06 = RuleKey.of(REPO_KEY, "MyBatisMapperCheckRule06");
+    public static final RuleKey MYBATIS_MAPPER_CHECK_RULE_07 = RuleKey.of(REPO_KEY, "MyBatisMapperCheckRule07");
 
-    private void defineRulesForLanguage(Context context, String repositoryKey, String repositoryName,
-        String languageKey) {
-        NewRepository repository = context.createRepository(repositoryKey, languageKey).setName(repositoryName);
-
-        InputStream rulesXml = this.getClass().getResourceAsStream(rulesDefinitionFilePath());
-        if (rulesXml != null) {
-            RulesDefinitionXmlLoader rulesLoader = new RulesDefinitionXmlLoader();
-            rulesLoader.load(repository, rulesXml, StandardCharsets.UTF_8.name());
-        }
-
-        repository.done();
-    }
 
     @Override
     public void define(Context context) {
-        defineRulesForLanguage(context, REPO_KEY, REPO_NAME, Xml.KEY);
+        NewRepository repository = context.createRepository(REPO_KEY, Xml.KEY).setName(REPO_NAME);
+
+        NewRule mybatisRule01 = repository.createRule(MYBATIS_MAPPER_CHECK_RULE_01.rule())
+                .setName("select statement should not include 1=1")
+                .setHtmlDescription("select statement should not include 1=1, it is a useless code and bad practice, suggest removing it.")
+                .setTags("mybatis", "select")
+                .setType(RuleType.BUG)
+                .setSeverity(Severity.MINOR);
+        mybatisRule01.setDebtRemediationFunction(mybatisRule01.debtRemediationFunctions().linear("20min"));
+
+        NewRule mybatisRule02 = repository.createRule(MYBATIS_MAPPER_CHECK_RULE_02.rule())
+                .setName("update statement should not include 1=1")
+                .setHtmlDescription("update statement should not include 1=1, it is a useless code and bad practice, suggest removing it.")
+                .setTags("mybatis", "update")
+                .setType(RuleType.BUG)
+                .setSeverity(Severity.MAJOR);
+        mybatisRule02.setDebtRemediationFunction(mybatisRule01.debtRemediationFunctions().linear("20min"));
+
+        NewRule mybatisRule03 = repository.createRule(MYBATIS_MAPPER_CHECK_RULE_03.rule())
+                .setName("delete statement should not include 1=1")
+                .setHtmlDescription("delete statement should not include 1=1, it is a useless code and bad practice, suggest removing it.")
+                .setTags("mybatis", "delete")
+                .setType(RuleType.BUG)
+                .setSeverity(Severity.MAJOR);
+        mybatisRule03.setDebtRemediationFunction(mybatisRule01.debtRemediationFunctions().linear("20min"));
+
+        NewRule mybatisRule04 = repository.createRule(MYBATIS_MAPPER_CHECK_RULE_04.rule())
+                .setName("where condition not found in select statement")
+                .setHtmlDescription("If all parameters in the update statement of Mapper XML file are null,\n" +
+                        "            the sql will not have where condition, then it will select all records from the table, which may lead to\n" +
+                        "            performance issues. Suggest using simple parameters instead of elements (such as:\n" +
+                        "            if,choose,when,otherwise,trim,where,set,foreach elemnt etc.) for required parameters.")
+                .setTags("mybatis", "select")
+                .setType(RuleType.BUG)
+                .setSeverity(Severity.MINOR);
+        mybatisRule04.setDebtRemediationFunction(mybatisRule01.debtRemediationFunctions().linear("20min"));
+
+        NewRule mybatisRule05 = repository.createRule(MYBATIS_MAPPER_CHECK_RULE_05.rule())
+                .setName("where condition not found in update statement")
+                .setHtmlDescription("If all parameters in the update statement of Mapper XML file are null,\n" +
+                        "            the sql will not have where condition, then it will update all records from the table, which will result\n" +
+                        "            production accident. Suggest using simple parameters instead of elements (such as:\n" +
+                        "            if,choose,when,otherwise,trim,where,set,foreach elemnt etc.) for required parameters.")
+                .setTags("mybatis", "update")
+                .setType(RuleType.BUG)
+                .setSeverity(Severity.MAJOR);
+        mybatisRule05.setDebtRemediationFunction(mybatisRule01.debtRemediationFunctions().linear("20min"));
+
+        NewRule mybatisRule06 = repository.createRule(MYBATIS_MAPPER_CHECK_RULE_06.rule())
+                .setName("where condition not found in delete statement")
+                .setHtmlDescription("If all parameters in the delete statement of Mapper XML file are null,\n" +
+                        "            the sql will not have where condition, then it will delete all records from the table, which will result\n" +
+                        "            production accident. Suggest using simple parameters instead of elements (such as:\n" +
+                        "            if,choose,when,otherwise,trim,where,set,foreach elemnt etc.) for required parameters.")
+                .setTags("mybatis", "delete")
+                .setType(RuleType.BUG)
+                .setSeverity(Severity.CRITICAL);
+        mybatisRule06.setDebtRemediationFunction(mybatisRule01.debtRemediationFunctions().linear("30min"));
+
+        NewRule mybatisRule07 = repository.createRule(MYBATIS_MAPPER_CHECK_RULE_07.rule())
+                .setName("select statement should not include *")
+                .setHtmlDescription("select statement should not include *, it is not a bad practice, suggest using specific fields.")
+                .setTags("mybatis", "select")
+                .setType(RuleType.BUG)
+                .setSeverity(Severity.MINOR);
+        mybatisRule07.setDebtRemediationFunction(mybatisRule01.debtRemediationFunctions().linear("20min"));
+
+        repository.done();
     }
 
 }
