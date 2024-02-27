4bf2a6bf384f5a5b3355d36f8c6981d4e1968275
donghui <977675308@qq.com>
2024-02-27 23:06:30 +0800

    make constants final (#46)

--- src/main/java/org/sonarsource/plugins/mybatis/Constant.java
+++ src/main/java/org/sonarsource/plugins/mybatis/Constant.java
@@ -1,12 +1,12 @@
 package org.sonarsource.plugins.mybatis;
 
 public class Constant {
-    public static String MYBATIS_MAPPER_CHECK_RULE_01 = "MyBatisMapperCheckRule01";
-    public static String MYBATIS_MAPPER_CHECK_RULE_02 = "MyBatisMapperCheckRule02";
-    public static String MYBATIS_MAPPER_CHECK_RULE_03 = "MyBatisMapperCheckRule03";
-    public static String MYBATIS_MAPPER_CHECK_RULE_04 = "MyBatisMapperCheckRule04";
-    public static String MYBATIS_MAPPER_CHECK_RULE_05 = "MyBatisMapperCheckRule05";
-    public static String MYBATIS_MAPPER_CHECK_RULE_06 = "MyBatisMapperCheckRule06";
-    public static String MYBATIS_MAPPER_CHECK_RULE_07 = "MyBatisMapperCheckRule07";
+    public static final String MYBATIS_MAPPER_CHECK_RULE_01 = "MyBatisMapperCheckRule01";
+    public static final String MYBATIS_MAPPER_CHECK_RULE_02 = "MyBatisMapperCheckRule02";
+    public static final String MYBATIS_MAPPER_CHECK_RULE_03 = "MyBatisMapperCheckRule03";
+    public static final String MYBATIS_MAPPER_CHECK_RULE_04 = "MyBatisMapperCheckRule04";
+    public static final String MYBATIS_MAPPER_CHECK_RULE_05 = "MyBatisMapperCheckRule05";
+    public static final String MYBATIS_MAPPER_CHECK_RULE_06 = "MyBatisMapperCheckRule06";
+    public static final String MYBATIS_MAPPER_CHECK_RULE_07 = "MyBatisMapperCheckRule07";
 
 }
